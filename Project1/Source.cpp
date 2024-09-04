4acd2ca176f267a58f6ab70662c29111a615bb26
valentinaShturts <valentinashturts@gmail.com>
2024-09-04 14:48:17 +0300

    PerformAction

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -11,6 +11,19 @@ struct Employee
 	double payment;
 };
 
+void ClearFileContents(const string& filename)
+{
+	FILE* file = fopen("employee_list.txt", "w"); // Открытие файла для чтения
+	if (file != nullptr) // Открытие файла для записи (это удалит его содержимое)
+	{
+		fclose(file);
+	}
+	else
+	{
+		cout << "Error: Unable to open file." << endl;
+	}
+}
+
 void ShowMenu()
 {
 	cout << " 1. Show all employees " << endl << " 2. Search by surname " << endl << " 3. Search by payment " << endl << " 4. Sort by surnames " << endl << " 5. Add new employee " << endl << " 6. Delete employee " << endl << " 0. Exit " << endl;
@@ -30,6 +43,24 @@ void WriteEmployeeToFile(const Employee& employee)
 	}
 	else {
 		cout << "Error: Unable to open file for writing." << endl;
+	}
+}
+
+void ViewEmployeeListFromFile()
+{
+	FILE* file = fopen("employee_list.txt", "r"); // Открытие файла для чтения
+	if (file != nullptr)
+	{
+		char name[50], surname[50], phone[50], payment[50];
+		while (fscanf(file, "%49s %49s %49s %49s", name, surname, phone, payment) == 4)
+		{
+			cout << "Emplyee " << name << " " << surname << endl << "Phone: " << phone << endl << "Payment: " << payment;
+		}
+		fclose(file);
+	}
+	else
+	{
+		cout << "Error: Unable to open file for reading." << endl;
 	}
 }
 
@@ -66,18 +97,30 @@ Employee create()
 	return employee;
 }
 
+void PerformAction(int option)
+{
+	if (option == 1) ViewEmployeeListFromFile();
+	else if (option == 5) create();
+	else if (option == 0) exit(0);
+
+	int temp;
+	cout << "Enter smth to continue...";
+	cin >> temp;
+}
+
 int main()
 {
 	int option = 0;
+	ClearFileContents("employee_list.txt");
 	do
 	{
 		system("cls");
 		ShowMenu();
 		cout << " Enter your option: ";
 		cin >> option;
-		create();
-		
-	} while (true);
+		//system("cls");
+		PerformAction(option);
+	} while (option!=0);
 	
 	
 
