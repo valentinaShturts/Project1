403b60da6776ac1267602335520c05ca92264fe8
valentinaShturts <valentinashturts@gmail.com>
2024-09-04 12:42:16 +0300

    ShowMenu()3

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
-struct Worker
+struct Employee
 {
 	char* name;
 	char* surname;
-	char* number;
-
+	char* phone;
+	double payment;
 };
+
+void ShowMenu()
+{
+	cout << " 1. Show all employees " << endl << " 2. Search by surname " << endl << " 3. Search by payment " << endl << " 4. Sort by surnames " << endl << "";
+}
+
 int main()
 {
+	int option = 0;
+	
+	
 
+
+	return 0;
 }
